bca02f01c5fcc7ce08292647dbda368fefb397b1
kiks12 <francistolentino1107@gmail.com>
2022-07-27 10:15:11 +0800

    template rendering

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegistrationController } from './registration/registration.controller';
+import { RegistrationService } from './registration/registration.service';
+import { RegistrationModule } from './registration/registration.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RegistrationModule],
+  controllers: [AppController, RegistrationController],
+  providers: [AppService, RegistrationService],
 })
 export class AppModule {}
