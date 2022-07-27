d2b403bcfbd87f1eb944852f9acfa4ed0cfd832f
kiks12 <francistolentino1107@gmail.com>
2022-07-27 12:26:45 +0800

    js not working

---
+++ public/js/registration.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+const passwordInput = document.getElementById('password');
+const passwordCheckBox = document.getElementById('passwordCheckBox');
+
+passwordCheckBox.addEventListener('change', (e) => {
+  console.log(e);
+});
